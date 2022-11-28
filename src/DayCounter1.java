11be72fcfcba2ee7770bb80e410c4dcd99f10a40
Marcin <lmartino@wp.pl>
2022-11-28 06:46:56 +0100

    nowe

--- src/DayCounter1.java
+++ src/DayCounter1.java
@@ -23,7 +23,38 @@ public class DayCounter1 {
 
     // TODO zaimplementować metodę
     static int countDays(int month, int year) {
-        return 0; // będzie musiałą coś zwracać - na razie 0
+        int count = -1;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 10:
+            case 12:
+                count = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                count = 30;
+                break;
+            case 2:
+                if (year % 4 == 0) {
+                    count = 29;
+                } else {
+                    count = 28;
+                }
+                // TODO sprawdzić logikę
+                if ((year % 100 == 0) && (year % 400 != 0)) {
+                    count = 28;
+
+            }
+
+
+
+        }
+        return count; // będzie musiałą coś zwracać - na razie 0
 
     }
 
